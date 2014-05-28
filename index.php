83abad3665f06b8df42d68547a94275fa21f5612
Rômulo <romulo1984@gmail.com>
2014-05-28 08:16:13 -0300

    Alteração no title

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="author" content="">
     <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
 
-    <title>Protótipo - Os Convidados</title>
+    <title>Protótipo - Os Convidados [ DEHBORA ]</title>
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
